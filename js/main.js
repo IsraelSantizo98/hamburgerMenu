2f05ab68b2a947241946bb44f74b3c8cd7a8aaab
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-07-08 22:07:48 -0600

    Last Commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ const $modal = document.getElementById("modal");
 const $close = document.getElementById("close");
 const $hideButton = document.getElementById("hide-modal");
 const $closeButton = document.getElementById("close-modal");
+//
+const ipad = window.matchMedia("screen and (max-width: 768px)");
+const menu = document.querySelector(".menu");
+const burgerButton = document.querySelector("#burger-menu");
+ipad.addListener(validation);
+validation(ipad);
+function validation(event) {
+  if (event.matches) {
+    burgerButton.addEventListener("click", hideShow);
+    // console.log(event);
+  } else {
+    burgerButton.removeEventListener("click", hideShow);
+  }
+}
+function hideShow() {
+  if (menu.classList.contains("is_active")) {
+    menu.classList.remove("is_active");
+  } else {
+    menu.classList.add("is_active");
+  }
+}
+//
 $icon.addEventListener("click", (event) => {
   console.log(event);
   $overlay.style.transition = "1.1s ease-in";
@@ -18,7 +40,7 @@ $button.addEventListener("click", (event) => {
 });
 $close.addEventListener("click", (event) => {
   $overlay.style.transition = "1.1s ease-in";
-  $modal.style.animation = "animationOut .8s forwards";
+  $modal.style.animation = "animationFalse .8s forwards";
   $overlay.classList.remove("active");
 });
 $overlay.addEventListener("click", (event) => {
